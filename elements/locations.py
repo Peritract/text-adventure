54a93e5d03fba2cf2e38aa40f23b348b5d88eca8
Peritract <peritract@hotmail.co.uk>
2019-10-19 13:12:33 +0100

    doc strings

--- elements/locations.py
+++ elements/locations.py
@@ -1,9 +1,16 @@
+"""Classes for in-game locations."""
+
+
 class Location():
+    """The base location class."""
+
     def __init__(self, name, description, coordinates, exits=[]):
+        """Constructor method for the base location class."""
         self.name = name
         self.description = description
         self.coordinates = coordinates
         self.exits = exits
-        
+
     def __str__(self):
+        """Returns the name & description."""
         return f"{self.name} - {self.description}"
--- elements/test_locations.py
+++ elements/test_locations.py
@@ -2,15 +2,17 @@
 
 from .locations import Location
 
+
 def test_location_constructor():
     """Tests that Locations are instantiated correctly."""
-    test_obj = Location("place", "a place", (0,0,0), [])
+    test_obj = Location("place", "a place", (0, 0, 0), [])
     assert test_obj.name == "place"
     assert test_obj.description == "a place"
-    assert test_obj.coordinates == (0,0,0)
+    assert test_obj.coordinates == (0, 0, 0)
     assert test_obj.exits == []
-    
+
+
 def test_location_string():
     """Tests that Locations __str__ method returns correctly."""
-    test_obj = Location("Hovel", "a drab and dingy room", (0,0,0))
+    test_obj = Location("Hovel", "a drab and dingy room", (0, 0, 0))
     assert test_obj.__str__() == "Hovel - a drab and dingy room"
