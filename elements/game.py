a8a7d466fa95e03b2805638785be41e9abfa29d8
Peritract <peritract@hotmail.co.uk>
2019-10-19 13:22:54 +0100

    game class

---
+++ elements/game.py
@@ -0,0 +1,12 @@
+"""This file contains the implementation of the Game class"""
+
+
+class Game():
+    """The Game class"""
+
+    def __init__(self):
+        """Constructor method for the Game class"""
+        pass
+
+    def main_menu(self):
+        pass
