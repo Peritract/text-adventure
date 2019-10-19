2c166933395ae01e2f80169e47663ccb73382d03
Peritract <peritract@hotmail.co.uk>
2019-10-19 14:04:03 +0100

    explanation

--- __main__.py
+++ __main__.py
@@ -3,5 +3,7 @@
 from .tools.display_methods import display
 from .elements.locations import Location
 
+# hacks to test concepts - will be replaced when there is real functionality
 a = Location("place", "a place", (0, 0, 0), [])
-display("Hello World!")
+display(a.__str__())
+print(a)
